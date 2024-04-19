3b5ed1dc5b306dbe1f189e24680be0c4f76f6b0a
Victoriia Oleh <slavika86@gmail.com>
2024-04-19 17:55:02 +0300

    add task basic one

---
+++ src/basic/1.ts
@@ -0,0 +1,14 @@
+// const age = 50;
+// const name = 'Max';
+// const toggle = true;
+// const empty = null;
+// const callback = (a) => { return 100 + a };
+
+let age: number = 50;
+let nameUser: string = 'Max';
+let toggle: boolean = true;
+let empty: null = null;
+const callback = (a: number) => { return 100 + a };
+
+console.log(callback(12));
+console.log(nameUser);
