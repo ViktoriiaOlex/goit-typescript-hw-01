30eb0f4248b7dbf2c1f3d39c5522fbcb9a1a4436
Victoriia Oleh <slavika86@gmail.com>
2024-04-22 11:00:22 +0300

    correct generics task 2 & 3

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,5 +1,9 @@
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+// function merge (objA, objB) {
+//     return Object.assign(objA, objB);
+//   }
+
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
 
 //Приклад для перевіки
